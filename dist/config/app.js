f46470386f54511e4dbdf15940bf090309b7e089
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-04-09 16:38:55 +0700

    migrate to typescript

---
+++ dist/config/app.js
@@ -0,0 +1,5 @@
+import express from "express";
+import cors from 'cors';
+const app = express();
+app.use(cors());
+export default app;
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+import app from "./config/app.js";
+import apiRoutes from "./routes/api.js";
+import responser from "./app/utils/apiResponser.js";
+import bodyParser from "body-parser";
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
+app.use('/', (req, res) => {
+    return responser.error(res, {
+        code: 404,
+        message: 'Not Found'
+    });
+});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.info(`App started on port ${port}`);
+});
